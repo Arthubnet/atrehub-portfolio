60e53e55c7d3756bd8bda767b070ec18e62b8113
Lena <z.otig@yahoo.com>
2022-07-03 20:39:44 -0600

    fake2

--- src/Components/body/about/about-page.component.jsx
+++ src/Components/body/about/about-page.component.jsx
@@ -8,13 +8,13 @@ function AboutPage() {
     <div className="body__about">
       <h2>React Web Developer</h2>
       <p>
-        My name is Artem Trehub, I'm a web-developer with 12+ years of
+        {`My name is Artem Trehub, I'm a web-developer with 12+ years of
         experience in IT. Mostly creating Web Apps on React but also proficient
         with pure JavaScript, CSS and HTML. My path in Web started as a marketer
         so I also enjoy UX and everything related to user experience(interaction
         analysis, psychology). Always open and excited for a new project, reach
         out to me so we can create something exciting.
-        {`Here is fake ${process.env.REACT_APP_FAKE}`}
+        Here is fake ${process.env.REACT_APP_FAKE}`}
       </p>
       <div className="body__about__title">
         <a
